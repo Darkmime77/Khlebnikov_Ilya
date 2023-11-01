6350da559f7607aefa680706354c36f07ba35a4f
Darkmime77 <ilya_hleb@mail.ru>
2023-11-02 01:14:51 +0500

    FIO

--- Хлебников Илья/database.py
+++ Хлебников Илья/database.py
@@ -2,7 +2,7 @@ from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 
-SQLALCHEMY_DATABASE_URL = "postgresql://postgres:PASS@localhost/LOGIN"
+SQLALCHEMY_DATABASE_URL = "postgresql://postgres:Darkmime77@localhost/Volga-IT"
 # SQLALCHEMY_DATABASE_URL = "postgresql://user:password@postgresserver/db"
 
 engine = create_engine(
